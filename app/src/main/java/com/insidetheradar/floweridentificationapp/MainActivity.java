e2d7a1b39fe2052769a995111f531f1221f21ed9
Vivek Pandey <52068103+insidetheradar@users.noreply.github.com>
2019-10-30 13:47:09 +0530

    UI for the app

---
+++ app/src/main/java/com/insidetheradar/floweridentificationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.insidetheradar.floweridentificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
